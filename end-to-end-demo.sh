1226cccd08be250ef28c655e1c823fa377146e39
ksrcad1 <ksrcad1@gmail.com>
2024-06-17 11:40:12 -0400

    shell scripting

--- end-to-end-demo.sh
+++ end-to-end-demo.sh
@@ -38,3 +38,14 @@ for str in ${MYARRAY[@]}; do #declaration
     exit 1
   fi
 done
+
+NUMBER=$1
+
+if [ $NUMBER -gt 10 ]
+then
+    echo "Given number $NUMBER is greater than 10"
+else
+    echo "Given number $NUMBER is less than 10" 
+fi
+
+#-gt, -lt, -eq, -ge, -le
