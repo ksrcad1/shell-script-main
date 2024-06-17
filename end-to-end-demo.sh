75b2057831b3d39a159cc4ac54739547fae19aec
ksrcad1 <ksrcad1@gmail.com>
2024-06-17 08:29:31 -0400

    end to end script

---
+++ end-to-end-demo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#syntax to print the messages directly
+echo "This is a Direct message"
+#store the message in variable and the print
+MESSAGE="This message is form the MESSAGE variable"
+echo "using:: fomat-1 \$MESSAGE :: $MESSAGE "
+echo "using:: fomat-2 \${MESSAGE} :: ${MESSAGE}"
