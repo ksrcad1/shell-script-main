0c0f49646785c5e54b76867775e2e56408c941a7
ksrcad1 <ksrcad1@gmail.com>
2024-06-17 09:48:24 -0400

    eited end to end demo

--- end-to-end-demo.sh
+++ end-to-end-demo.sh
@@ -19,11 +19,11 @@ echo "Username1 is: $USERNAME1, and Username2 is: $USERNAME2 , and Username3 is:
 
 #array in shell script
 #just like in the programming languages , you can use arrays in shell scripts.
-MYARRAY=("element1" "element2" "element3" "element1") #declaration
+MYARRAY=("element1" "element2" "element3" "element4") #declaration
 # to access array elements, you can loop through the array elements or loop through indicies.
 echo "the element at index-0 is: ${MYARRAY[0]}"
 echo "All elements of and array can be printed using 'array[@]' : ${MYARRAY[@]}"
 echo "looping thorugh the array elements"
-for str in ${MYARRAY[@]}; #declaration
-    echo "${str}"
+for str in ${MYARRAY[@]}; do #declaration
+  echo $str
 done
