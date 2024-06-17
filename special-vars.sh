59f2e45abb778d3bfdc0874745554a52ff02c1ca
ksrcad1 <ksrcad1@gmail.com>
2024-06-17 11:30:53 -0400

    shell scripting

--- special-vars.sh
+++ special-vars.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "All variables: $@"
-echo "Number of variables passed: $#"
-echo "Script Name: $0"
-echo "Current working directory: $PWD"
-echo "Home directory of current user: $HOME"
-echo "Which user is running this script: $USER"
-echo "Hostname: $HOSTNAME"
-echo "Process ID of the current shell script: $$"
+echo "All variables using \$@: $@"
+echo "Number of variables passed using \$#: $#"
+echo "Script Name using \$0: $0"
+echo "Current working directory using \$PWD: $PWD"
+echo "Home directory of current user using \$HOME: $HOME"
+echo "Which user is running this script\$USER: $USER"
+echo "Hostname using \$HOSTNAME variable: $HOSTNAME"
+echo "Process ID of the current shell script uisng \$$: $$"
 sleep 60 &
-echo "Process ID of last background command: $!"
+echo "Process ID of last background command \$!: $!"
