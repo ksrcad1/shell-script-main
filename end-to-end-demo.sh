9343c274b785eb042b769bfe1ae07e97a4110215
ksrcad1 <ksrcad1@gmail.com>
2024-06-17 09:30:57 -0400

    eited end to end demo

--- end-to-end-demo.sh
+++ end-to-end-demo.sh
@@ -13,6 +13,17 @@ echo "Please enter username1::"
 read -s USERNAME1  # -s slient, what ever the user enters will be stored in the USERNAME avriable
 read -p "Please enter username2::" USERNAME2  # single line for prompt and entering value.
 # -p will not make it silent, so use both
-read -ps "Please enter username3::" USERNAME3 # -ps throws error, it is -sp, silent first, then prompt
+read -sp "Please enter username3::" USERNAME3 # -ps throws error, it is -sp, silent first, then prompt
 echo "Username1 is: $USERNAME1, and Username2 is: $USERNAME2 , and Username3 is: $USERNAME3"
 #https://ryanstutorials.net/bash-scripting-tutorial/bash-input.php
+
+#array in shell script
+#just like in the programming languages , you can use arrays in shell scripts.
+MYARRAY=("element1" "element2" "element3" "element1") #declaration
+# to access array elements, you can loop through the array elements or loop through indicies.
+echo "the element at index-0 is: ${MYARRAY[0]}"
+echo "All elements of and array can be printed using 'array[@]' : ${MYARRAY[0]}"
+echo "looping thorugh the array elements"
+for str in ${MYARRAY[@]}; declaration
+  echo $str
+done
