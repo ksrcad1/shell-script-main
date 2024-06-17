7d80cecd5bbf015df037bbb6dbadb8d36e59330a
ksrcad1 <ksrcad1@gmail.com>
2024-06-17 09:08:47 -0400

    eited end to end demo

--- end-to-end-demo.sh
+++ end-to-end-demo.sh
@@ -13,5 +13,6 @@ echo "Please enter username1::"
 read -s USERNAME1  # -s slient, what ever the user enters will be stored in the USERNAME avriable
 read -p "Please enter username2::" USERNAME2  # single line for prompt and entering value.
 # -p will not make it silent, so use both
-read -p  -s "Please enter username3::" USERNAME3
+read -ps "Please enter username3::" USERNAME3
 echo "Username1 is: $USERNAME1, and Username2 is: $USERNAME2 , and Username3 is: $USERNAME3"
+#https://ryanstutorials.net/bash-scripting-tutorial/bash-input.php
