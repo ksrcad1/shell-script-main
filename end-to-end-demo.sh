ed91fbc76b0e3d033d8aeeca94dcdf827f994622
ksrcad1 <ksrcad1@gmail.com>
2024-06-17 09:04:54 -0400

    eited end to end demo

--- end-to-end-demo.sh
+++ end-to-end-demo.sh
@@ -9,8 +9,9 @@ echo "using:: fomat-1 \$MESSAGE :: $MESSAGE "
 echo "using:: fomat-2 \${MESSAGE} :: ${MESSAGE}"
 
 #read the values from the command line as input
-echo "Please enter username::"
-read -s USERNAME1  # what ever the user enters will be stored in the USERNAME avriable
+echo "Please enter username1::"
+read -s USERNAME1  # -s slient, what ever the user enters will be stored in the USERNAME avriable
 read -p "Please enter username2::" USERNAME2  # single line for prompt and entering value.
-
-echo "Username1 is: $USERNAME1, and Username2 is: $USERNAME2"
+# -p will not make it silent, so use both
+read -p  -s "Please enter username3::" USERNAME3
+echo "Username1 is: $USERNAME1, and Username2 is: $USERNAME2 , and Username3 is: $USERNAME3"
