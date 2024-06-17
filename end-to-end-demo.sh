569f023d62ece67f009fb1e251daab7eb9867088
ksrcad1 <ksrcad1@gmail.com>
2024-06-17 08:39:57 -0400

    more scripting

--- end-to-end-demo.sh
+++ end-to-end-demo.sh
@@ -2,7 +2,15 @@
 
 #syntax to print the messages directly
 echo "This is a Direct message"
-#store the message in variable and the print
+#declare a variable and store the message in variable and the print
 MESSAGE="This message is form the MESSAGE variable"
+#referring variable
 echo "using:: fomat-1 \$MESSAGE :: $MESSAGE "
 echo "using:: fomat-2 \${MESSAGE} :: ${MESSAGE}"
+
+#read the values from the command line as input
+echo "Please enter username::"
+read -s USERNAME1  # what ever the user enters will be stored in the USERNAME avriable
+read -p "Please enter username2::" USERNAME2  # single line for prompt and entering value.
+
+echo "Username1 is: $USERNAME1, and Username2 is: $USERNAME2"
